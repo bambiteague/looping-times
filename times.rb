e42d285e15183bf1a0a58b1f47dbcd63cf055218
bambi teague <teeegsa@gmail.com>
2020-12-07 14:45:26 -0600

    commit tree 726c13f0642fd78e9092d93539400ad6226c1e6c

--- times.rb
+++ times.rb
@@ -1,9 +1,15 @@
-def using_times
-	7.times do
-		puts "Wingardium Leviosa"
-	end	
-end
+# def using_times
+# 	7.times do
+# 		puts "Wingardium Leviosa"
+# 	end	
+# end
 
+
+def using_times
+	7.times do 
+		puts "Wingardium Leviosa"
+	end
+end
 
 
 
